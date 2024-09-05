3554f256d279cb5f469cecd69d34929ff68d24b6
egger-cléa-emf <clea.egger@studentfr.ch>
2024-09-05 09:55:45 +0200

    exercice03

---
+++ exercices/exercice03/exercice03.java
@@ -0,0 +1,15 @@
+package exercices.exercice03;
+
+public class exercice03 {
+
+        public static void main(String[] args) {
+            int age = 16;
+            String nom = "Cléa";
+            
+            System.out.println ("Je m'appelle "+ nom" et j'ai "+ age" ans.");
+    
+        }
+    
+        
+    } 
+
