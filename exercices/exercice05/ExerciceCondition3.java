0670ff239a80a66e8ec127b60fd610a6119d270b
egger-cléa-emf <clea.egger@studentfr.ch>
2024-09-12 11:40:54 +0200

    exercice5 en cours

---
+++ exercices/exercice05/ExerciceCondition3.java
@@ -0,0 +1,16 @@
+package exercices.exercice05;
+
+public class ExerciceCondition3 {
+    public static void main(String[] args) {
+        int valeur1=-1;
+        int valeur2=5;
+        int valeur3=valeur1*valeur2;
+
+        if (valeur3<0) {
+            System.out.println("Le résultat est négatif");
+        }
+        else {
+            System.out.println("Le résultat est positif");
+        }
+    }
+}
