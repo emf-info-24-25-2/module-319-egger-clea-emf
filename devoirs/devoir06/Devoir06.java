bdf1a343dab60847c514f253c49577ceef78c01e
egger-cléa-emf <clea.egger@studentfr.ch>
2024-11-14 08:25:08 +0100

    devoir06

--- devoirs/devoir06/Devoir06.java
+++ devoirs/devoir06/Devoir06.java
@@ -1,29 +1,81 @@
-public class devoir06;
+import java.util.Random;
 
-public final static int TAILLE_TABLEAU = 20;
-public final static int VALEUR_MIN = 0;
-public final static int VALEUR_MAX = 50;
-public final static int VALEUR_RECHERCHEE = 7;
+public class Devoir06;
 
-public static int[] genereTableau (int min, int max, int taille);{
+    // Définition des constantes
+    private static final int TAILLE_TABLEAU = 20;
+    private static final int VALEUR_MIN = 0;
+    private static final int VALEUR_MAX = 50;
+    private static final int VALEUR_RECHERCHEE = 7;
 
-    return
-}
+    // Méthode pour générer un tableau avec des valeurs aléatoires
+    public static int[] genereTableau(int min, int max, int taille) {
+        int[] tableau = new int[taille];
+        Random random = new Random();
+        for (int i = 0; i < taille; i++) {
+            tableau[i] = random.nextInt((max - min) + 1) + min;
+        }
+        return tableau;
+    }
 
-public static int rechercheMin (int [] tableau);{
+    // Méthode pour trouver la valeur minimale dans un tableau
+    public static int rechercheMin(int[] tableau) {
+        int min = tableau[0];
+        for (int valeur : tableau) {
+            if (valeur < min) {
+                min = valeur;
+            }
+        }
+        return min;
+    }
 
-}
+    // Méthode pour trouver la valeur maximale dans un tableau
+    public static int rechercheMax(int[] tableau) {
+        int max = tableau[0];
+        for (int valeur : tableau) {
+            if (valeur > max) {
+                max = valeur;
+            }
+        }
+        return max;
+    }
 
-public static int rechercheMax (int [] tableau);{
+    // Méthode pour rechercher une valeur spécifique dans un tableau
+    public static int rechercheValeur(int[] tableau, int valeur) {
+        for (int i = 0; i < tableau.length; i++) {
+            if (tableau[i] == valeur) {
+                return i; // retourne l'index si la valeur est trouvée
+            }
+        }
+        return -1; // retourne -1 si la valeur n'est pas trouvée
+    }
 
-}
+    // Méthode principale
+    public static void main(String[] args) {
+        // Génération du tableau avec les constantes définies
+        int[] tableau = genereTableau(VALEUR_MIN, VALEUR_MAX, TAILLE_TABLEAU);
 
-public static int recherhceValeur (int[] tableau, int valeur) {
-    
-}
+        // Affichage du contenu du tableau
+        System.out.print("Tableau généré : ");
+        for (int valeur : tableau) {
+            System.out.print(valeur + " ");
+        }
+        System.out.println();
 
-public static void main(String[] args) {
-    int[] tableau = genereTableau(VALEUR_MIN, VALEUR_MAX, TAILLE_TABLEAU);
-    
-    System.out.println(tableau);
-}
+        // Recherche et affichage du minimum
+        int min = rechercheMin(tableau);
+        System.out.println("Valeur minimale dans le tableau : " + min);
+
+        // Recherche et affichage du maximum
+        int max = rechercheMax(tableau);
+        System.out.println("Valeur maximale dans le tableau : " + max);
+
+        // Recherche et affichage de l'index de VALEUR_RECHERCHEE
+        int index = rechercheValeur(tableau, VALEUR_RECHERCHEE);
+        if (index != -1) {
+            System.out.println("La valeur " + VALEUR_RECHERCHEE + " se trouve à l'index : " + index);
+        } else {
+            System.out.println("La valeur " + VALEUR_RECHERCHEE + " n'est pas présente dans le tableau.");
+        }
+    }
+
